d1fbe78264685a79b157f172ae725a3430ab8407
Ralph Meier <ralph_meier@swissonline.ch>
2011-06-10 17:05:40 +0200

    prepared info banner, not visible yet

--- js/engine.stats.js
+++ js/engine.stats.js
@@ -1,4 +1,4 @@
-engine.stats = new Stats();
+//engine.stats = new Stats();
 
 // Align top-left
 engine.stats.domElement.style.position = 'absolute';
@@ -6,6 +6,7 @@ engine.stats.domElement.style.left = '0px';
 engine.stats.domElement.style.top = '0px';
 
 document.body.appendChild( engine.stats.domElement );
+//document.body.appendChild( engine.stats );
 
 setInterval( function () {
 
