9a8ff6623cd2e697652def1c7502a6f21a76a71f
Ralph Meier <ralph_meier@swissonline.ch>
2011-06-11 20:07:27 +0200

    refactoring of player collision to new block

--- test/engine.playerSpec.js
+++ test/engine.playerSpec.js
@@ -1,8 +1,9 @@
 describe('Player', function () {
-    var currentStone = [];
+    var currentShape = [];
     var currentMap = [];
+    var currentBlock;
     beforeEach(function(){
-        currentStone = [
+        currentShape = [
         [0,1,0],
         [0,1,0],
         [1,1,0],
@@ -28,34 +29,35 @@ describe('Player', function () {
         ];
         engine.screen.tileCountX = 16;
         engine.screen.tileCountY = 16;
+        currentBlock = new Tetris.Block(currentShape, engine.player.tileX, engine.player.tileY);
     });
     
     describe('Collision right', function () {
         it('should return true if collided at right border', function () {
             engine.player.tileX = 14;
             engine.player.tileY = 0;
-            var collided = engine.player.collide(currentStone, currentMap, 1,0);
+            var collided = engine.player.collide(currentBlock, currentMap, 1,0);
             expect(collided).toBeTruthy();
         });
 
         it('should return true if colliding in the map 1', function () {
             engine.player.tileX = 2;
             engine.player.tileY = 6;
-            var collided = engine.player.collide(currentStone, currentMap, 1,0);
+            var collided = engine.player.collide(currentBlock, currentMap, 1,0);
             expect(collided).toBeTruthy();
         });
 
         it('should return true if colliding in the map 2', function () {
             engine.player.tileX = 2;
             engine.player.tileY = 3;
-            var collided = engine.player.collide(currentStone, currentMap, 1,0);
+            var collided = engine.player.collide(currentBlock, currentMap, 1,0);
             expect(collided).toBeTruthy();
         });
 
         it('should return false if not colliding', function () {
             engine.player.tileX = 1;
             engine.player.tileY = 0;
-            var collided = engine.player.collide(currentStone, currentMap, 1,0);
+            var collided = engine.player.collide(currentBlock, currentMap, 1,0);
             expect(collided).toBeFalsy();
         });
     });
@@ -64,28 +66,28 @@ describe('Player', function () {
         it('should return true if collided at left border', function () {
             engine.player.tileX = 0;
             engine.player.tileY = 0;
-            var collided = engine.player.collide(currentStone, currentMap, -1,0);
+            var collided = engine.player.collide(currentBlock, currentMap, -1,0);
             expect(collided).toBeTruthy();
         });
 
         it('should return true if colliding in the map 1', function () {
             engine.player.tileX = 5;
             engine.player.tileY = 3;
-            var collided = engine.player.collide(currentStone, currentMap, -1,0);
+            var collided = engine.player.collide(currentBlock, currentMap, -1,0);
             expect(collided).toBeTruthy();
         });
 
         it('should return true if colliding in the map 2', function () {
             engine.player.tileX = 4;
             engine.player.tileY = 6;
-            var collided = engine.player.collide(currentStone, currentMap, -1,0);
+            var collided = engine.player.collide(currentBlock, currentMap, -1,0);
             expect(collided).toBeTruthy();
         });
 
         it('should return false if not colliding', function () {
             engine.player.tileX = 1;
             engine.player.tileY = 0;
-            var collided = engine.player.collide(currentStone, currentMap, -1,0);
+            var collided = engine.player.collide(currentBlock, currentMap, -1,0);
             expect(collided).toBeFalsy();
         });
     });
@@ -94,28 +96,28 @@ describe('Player', function () {
         it('should return true if collided at bottom border', function () {
             engine.player.tileX = 0;
             engine.player.tileY = 15;
-            var collided = engine.player.collide(currentStone, currentMap, 0, 1);
+            var collided = engine.player.collide(currentBlock, currentMap, 0, 1);
             expect(collided).toBeTruthy();
         });
 
         it('should return true if colliding in the map 1', function () {
             engine.player.tileX = 0;
             engine.player.tileY = 14;
-            var collided = engine.player.collide(currentStone, currentMap, 0, 1);
+            var collided = engine.player.collide(currentBlock, currentMap, 0, 1);
             expect(collided).toBeTruthy();
         });
 
         it('should return true if colliding in the map 2', function () {
             engine.player.tileX = 6;
             engine.player.tileY = 14;
-            var collided = engine.player.collide(currentStone, currentMap, 0, 1);
+            var collided = engine.player.collide(currentBlock, currentMap, 0, 1);
             expect(collided).toBeTruthy();
         });
 
         it('should return false if not colliding', function () {
             engine.player.tileX = 6;
             engine.player.tileY = 0;
-            var collided = engine.player.collide(currentStone, currentMap, 0, 1);
+            var collided = engine.player.collide(currentBlock, currentMap, 0, 1);
             expect(collided).toBeFalsy();
         });
     });
