f2eb9b506acea9bad58bd975cfc52521d50cf0f4
Ralph Meier <ralph_meier@swissonline.ch>
2011-07-01 19:44:27 +0200

    renmaed test files

--- js/Util/fps.js
+++ js/Util/fps.js
@@ -2,34 +2,29 @@ var gamejs = require('gamejs');
 var screen = require('Tetris/screen');
 
 exports.FpsDisplay = function() {
-   // fps counter
-   var lastDurations = [];
-   var fpsFont = new gamejs.font.Font();
-   var fpsWarningFont = new gamejs.font.Font('20px monospace');
-   var fpsAvg = 99;
-   this.lowCPU = false;
+    // fps counter
+    var lastDurations = [];
+    var fpsFont = new gamejs.font.Font();
+    var fpsWarningFont = new gamejs.font.Font('20px monospace');
+    var fpsAvg = 99;
+    this.lowCPU = false;
 
-   this.update = function(msDuration) {
-      // fps
-      lastDurations.push(msDuration);
-      if (lastDurations.length > 60) {
-         var sum = 0;
-         lastDurations = lastDurations.splice(0, 10);
-         lastDurations.forEach(function(ld) {
+    this.update = function(msDuration) {
+        // fps
+        lastDurations.push(msDuration);
+        var sum = 0;
+        lastDurations = lastDurations.splice(0, 10);
+        lastDurations.forEach(function(ld) {
             sum += ld;
-         });
-         fpsAvg = Math.ceil(1000 / (sum / lastDurations.length));
-      }
-   };
+        });
+        fpsAvg = Math.ceil(1000 / (sum / lastDurations.length));
+    };
 
-   this.draw = function(display) {
-      if (fpsAvg < 60) this.lowCPU = true;
+    this.draw = function(display) {
+        if (fpsAvg < 60) this.lowCPU = true;
+        display.blit(fpsFont.render('' + fpsAvg, '#ff33ff'), [screen.SCREEN_WIDTH - 100, 5]);
+        return;
+    };
 
-      if (this.lowCPU) {
-         display.blit(fpsFont.render('Low CPU mode! ' + fpsAvg, '#ff33ff'), [screen.SCREEN_WIDTH - 100, 5]);
-      }
-      return;
-   };
-
-   return this;
+    return this;
 };
