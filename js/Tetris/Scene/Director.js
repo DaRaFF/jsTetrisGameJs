80e0c1449d243cb4cbfe1b8072a33659cc74df0f
Ralph Meier <ralphmeier79@gmail.com>
2019-02-17 09:11:34 +0100

    chore: linting

--- js/Tetris/Scene/Director.js
+++ js/Tetris/Scene/Director.js
@@ -1,50 +1,50 @@
-var gamejs = require('gamejs');
-var screen = require('Tetris/screen').screen;
-var touch = require('Util/touch');
-var orientation = require('Util/orientation');
+const gamejs = require('gamejs')
+const screen = require('Tetris/screen').screen
+const touch = require('Util/touch')
+const orientation = require('Util/orientation')
 
-var Director = function () {
-    touch.init();
-    orientation.init();
-    var onAir = false;
-    var activeScene = null;
+const Director = function () {
+  touch.init()
+  orientation.init()
+  let onAir = false
+  let activeScene = null
 
-    function tick(msDuration) {
-        if (!onAir) return;
+  function tick (msDuration) {
+    if (!onAir) return
 
-        if (activeScene.handleEvent) {
-            gamejs.event.get().forEach(activeScene.handleEvent);
-        } else {
-            // throw all events away
-            gamejs.event.get();
-        }
-        if (activeScene.update) {
-            activeScene.update(msDuration);
-        }
-        if (activeScene.draw) {
-            activeScene.draw(display);
-        }
-        return;
-    };
+    if (activeScene.handleEvent) {
+      gamejs.event.get().forEach(activeScene.handleEvent)
+    } else {
+      // throw all events away
+      gamejs.event.get()
+    }
+    if (activeScene.update) {
+      activeScene.update(msDuration)
+    }
+    if (activeScene.draw) {
+      activeScene.draw(display)
+    }
+    return
+  }
 
 
-    this.start = function(scene) {
-        onAir = true;
-        this.replaceScene(scene);
-        return;
-    };
+  this.start = function (scene) {
+    onAir = true
+    this.replaceScene(scene)
+    return
+  }
 
-    this.replaceScene = function(scene) {
-        activeScene = scene;
-    };
+  this.replaceScene = function (scene) {
+    activeScene = scene
+  }
 
-    this.getScene = function() {
-        return activeScene;
-    };
-    screen.update();
-    var display = gamejs.display.setMode([screen.screen_width, screen.screen_height]);
-    gamejs.time.fpsCallback(tick, this, 100);
-    return this;
-};
+  this.getScene = function () {
+    return activeScene
+  }
+  screen.update()
+  const display = gamejs.display.setMode([screen.screen_width, screen.screen_height])
+  gamejs.time.fpsCallback(tick, this, 100)
+  return this
+}
 
-exports.Director = Director;
+exports.Director = Director
--- js/Util/orientation.js
+++ js/Util/orientation.js
@@ -3,16 +3,16 @@
  * @see http://www.sitepoint.com/iphone-development-12-tips-2/
  */
 
-var gamejs = require('gamejs');
-var screen = require('Tetris/screen').screen;
+const gamejs = require('gamejs')
+const screen = require('Tetris/screen').screen
 
-function orientationHandler(event){
-    screen.update();
-    gamejs.display.setMode([screen.screen_width, screen.screen_height]);
-   return;
-};
+function orientationHandler (event) {
+  screen.update()
+  gamejs.display.setMode([screen.screen_width, screen.screen_height])
+  return
+}
 
-exports.init = function(){
-   document.addEventListener("orientationchange", orientationHandler, true);
-   return;
-};
+exports.init = function () {
+  document.addEventListener('orientationchange', orientationHandler, true)
+  return
+}
