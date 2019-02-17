f6322aa701550114f7bb1c244388ef1575c50a1f
Ralph Meier <ralphmeier79@gmail.com>
2019-02-17 08:37:16 +0100

    code: lint GameScene

--- js/Tetris/Scene/GameScene.js
+++ js/Tetris/Scene/GameScene.js
@@ -1,63 +1,63 @@
-var gamejs = require('gamejs');
-var screen = require('Tetris/screen').screen;
-var Tetris = require('Tetris/Tetris').Tetris;
-var fps = require('Util/fps');
-var EndScene = require('Tetris/Scene/EndScene').EndScene;
+const gamejs = require('gamejs')
+const screen = require('Tetris/screen').screen
+const Tetris = require('Tetris/Tetris').Tetris
+const fps = require('Util/fps')
+const EndScene = require('Tetris/Scene/EndScene').EndScene
 
-var GameScene = function(director){
-    var fpsDisplay = new fps.FpsDisplay();
-    //handle input
-    this.handleEvent = function(event) {
-        //keyboard input
-        if( event.type == gamejs.event.KEY_DOWN){
-            if (event.key == gamejs.event.K_UP){
-                Tetris.game.player.input = "TURN";
-            }
-            if (event.key == gamejs.event.K_DOWN){
-                Tetris.game.player.input = "DOWN";
-            }
-            if (event.key == gamejs.event.K_RIGHT){
-                Tetris.game.player.input = "RIGHT";
-            }
-            if (event.key == gamejs.event.K_LEFT){
-                Tetris.game.player.input = "LEFT";
-            }
-        }
-        //mouse input
-        if(event.type === gamejs.event.MOUSE_DOWN) {
-            if( screen.getInnerHeight() * 2 / 3 <= event.pos[1]){
-                Tetris.game.player.input = "DOWN";
-                return;
-            }
-            if( screen.getInnerWidth() * 1 / 3 >= event.pos[0]){
-                Tetris.game.player.input = "LEFT";
-                return;
-            }
-            if( screen.getInnerWidth() * 1 / 3 <= event.pos[0] && screen.getInnerWidth() * 2 / 3 >= event.pos[0]){
-                Tetris.game.player.input = "TURN";
-                return;
-            }
-            Tetris.game.player.input = "RIGHT";
-        }
-    };
-    
-    this.update = function(msDuration){
-        screen.update();
-        Tetris.game.update();
-        fpsDisplay.update(msDuration);
-        if(Tetris.game.gameOver){
-            director.replaceScene(new EndScene(director));
-        }
+const GameScene = function (director) {
+  const fpsDisplay = new fps.FpsDisplay()
+  // handle input
+  this.handleEvent = function (event) {
+    // keyboard input
+    if (event.type === gamejs.event.KEY_DOWN) {
+      if (event.key === gamejs.event.K_UP) {
+        Tetris.game.player.input = 'TURN'
+      }
+      if (event.key === gamejs.event.K_DOWN) {
+        Tetris.game.player.input = 'DOWN'
+      }
+      if (event.key === gamejs.event.K_RIGHT) {
+        Tetris.game.player.input = 'RIGHT'
+      }
+      if (event.key === gamejs.event.K_LEFT) {
+        Tetris.game.player.input = 'LEFT'
+      }
     }
-    
-    this.draw = function(display){
-        display.clear();
-        Tetris.game.draw(display);
-        fpsDisplay.draw(display);
+    // mouse input
+    if (event.type === gamejs.event.MOUSE_DOWN) {
+      if( screen.getInnerHeight() * 2 / 3 <= event.pos[1]) {
+        Tetris.game.player.input = 'DOWN'
+        return
+      }
+      if (screen.getInnerWidth() * 1 / 3 >= event.pos[0]) {
+        Tetris.game.player.input = 'LEFT'
+        return
+      }
+      if (screen.getInnerWidth() * 1 / 3 <= event.pos[0] && screen.getInnerWidth() * 2 / 3 >= event.pos[0]) {
+        Tetris.game.player.input = 'TURN'
+        return
+      }
+      Tetris.game.player.input = 'RIGHT'
     }
+  }
 
-    Tetris.game = new Tetris();
-    return this;
+  this.update = function (msDuration) {
+    screen.update()
+    Tetris.game.update()
+    fpsDisplay.update(msDuration)
+    if (Tetris.game.gameOver) {
+      director.replaceScene(new EndScene(director))
+    }
+  }
+
+  this.draw = function (display) {
+    display.clear()
+    Tetris.game.draw(display)
+    fpsDisplay.draw(display)
+  }
+
+  Tetris.game = new Tetris()
+  return this
 }
 
-exports.GameScene = GameScene;
+exports.GameScene = GameScene
