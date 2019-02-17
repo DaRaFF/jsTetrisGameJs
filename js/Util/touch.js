0e7b5a9eeebd7843929f35a89f7aa308bf6beb20
Ralph Meier <ralphmeier79@gmail.com>
2019-02-17 09:06:58 +0100

    chore: linting

--- js/Util/touch.js
+++ js/Util/touch.js
@@ -4,37 +4,37 @@
  * @see http://ross.posterous.com/2008/08/19/iphone-touch-events-in-javascript/
  */
 
-var EVENT_MAPPING = {
-   touchstart: 'mousedown',
-   touchmove: 'mousemove',
-   touchend: 'mouseup'
-};
+const EVENT_MAPPING = {
+  touchstart: 'mousedown',
+  touchmove: 'mousemove',
+  touchend: 'mouseup'
+}
 
-function touchHandler(event){
-   var touches = event.changedTouches;
-   var first = touches[0];
-   var type = EVENT_MAPPING[event.type];
+function touchHandler (event) {
+  const touches = event.changedTouches
+  const first = touches[0]
+  const type = EVENT_MAPPING[event.type]
 
-   var simulatedEvent = document.createEvent("MouseEvent");
-   simulatedEvent.initMouseEvent(type, true, true, window, 1,
-                              first.screenX, first.screenY,
-                              first.clientX, first.clientY, false,
-                              false, false, false, 0/*left*/, null);
+  const simulatedEvent = document.createEvent('MouseEvent')
+  simulatedEvent.initMouseEvent(type, true, true, window, 1,
+    first.screenX, first.screenY,
+    first.clientX, first.clientY, false,
+    false, false, false, 0 /* left */, null)
 
-   first.target.dispatchEvent(simulatedEvent);
-   if (event.type === 'touchmove'){
-       event.preventDefault();
-       window.scroll(0,0);
-       return;
-   } 
-   return;
-};
+  first.target.dispatchEvent(simulatedEvent)
+  if (event.type === 'touchmove') {
+    event.preventDefault()
+    window.scroll(0, 0)
+    return
+  }
+  return
+}
 
-exports.init = function(){
-   document.body.style['-webkit-touch-callout'] = 'none';
-   document.addEventListener("touchstart", touchHandler, true);
-   document.addEventListener("touchmove", touchHandler, true);
-   document.addEventListener("touchend", touchHandler, true);
-   document.addEventListener("touchcancel", touchHandler, true);
-   return;
-};
+exports.init = function () {
+  document.body.style['-webkit-touch-callout'] = 'none'
+  document.addEventListener('touchstart', touchHandler, true)
+  document.addEventListener('touchmove', touchHandler, true)
+  document.addEventListener('touchend', touchHandler, true)
+  document.addEventListener('touchcancel', touchHandler, true)
+  return
+}
